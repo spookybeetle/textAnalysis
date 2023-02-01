a3b7899915a48f30b1e49afea609f0cd0a0c9a1f
spookybeetle <krw5520@psu.edu>
2023-02-01 13:09:08 -0500

    Fix python and pycharm

--- homework/homework/python/main.py
+++ homework/homework/python/main.py
@@ -1,10 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
 import spacy
-# Need line 8 the first time: Then comment it out after the first time you run it:
+
 nlp = spacy.cli.download("en_core_web_sm")
 nlp = spacy.load('en_core_web_sm')
 
+AFAB = open('AFAB.txt', 'r', encoding='utf8')
+words = AFAB.read()
+wordstrings = str(words)
+print(wordstrings)
 
-doc = nlp("I cannot get it to open my text files. Am I missing something?")
-for token in doc:
-    print(token.text, token.pos_, token.dep_)
+AFAB = nlp(wordstrings)
+for token in AFAB:
+    # if token.pos_ == "VERB":
+    print(token.text, "---->", token.pos_, ":::::", token.lemma_)
+
+
 
