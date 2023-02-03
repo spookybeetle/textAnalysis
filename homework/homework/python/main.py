7c38eb762c4782d2038f166e85fb3e0ffbfd6a91
spookybeetle <krw5520@psu.edu>
2023-02-03 12:31:31 -0500

    NLP 2 files

--- homework/homework/python/main.py
+++ homework/homework/python/main.py
@@ -8,15 +8,46 @@ import spacy
 nlp = spacy.cli.download("en_core_web_sm")
 nlp = spacy.load('en_core_web_sm')
 
-AFAB = open('AFAB.txt', 'r', encoding='utf8')
-words = AFAB.read()
-wordstrings = str(words)
-print(wordstrings)
+import os
 
-AFAB = nlp(wordstrings)
-for token in AFAB:
-    # if token.pos_ == "VERB":
-    print(token.text, "---->", token.pos_, ":::::", token.lemma_)
+workingDir = os.getcwd()
+print("current working directory: " + workingDir)
+
+CollPath = os.path.join(workingDir, 'python-lang')
+print(CollPath)
+
+
+def readTextFiles(filepath):
+    with open(filepath, 'r', encoding='utf8') as f:
+        readFile = f.read()
+        stringFile = (readFile)
+        lengthFile = len(readFile)
+        print(lengthFile)
+
+tokens = nlp(stringFile)
+vectors = tokens.vector
+
+wordOfInterest = "cat"
+
+highSimilarityDict = {}
+for token in tokens:
+        if(token and token.vector_norm):
+            if wordOfInterest.similarity(token) > .3:
+                    highSimilarityDict[token] = wordOfInterest.similarity(token)
+
+highSimilarityReduced = {}
+for key, value in highSimilarityDict.items():
+        if value not in highSimilarityReduced.values():
+                highSimilarityReduced[key] = value
+        print(highSimilarityReduced)
+        print(len(highSimilarityReduced.items()), " vs ", len(highSimilarityDict.items()))
+
+for file in os.listdir(CollPath):
+    if file.endswith(".txt"):
+        filepath = f"{CollPath}/{file}"
+        print(filepath)
+        readTextFiles(filepath)
+
 
 
 
